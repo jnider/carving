d4ce257b33b1b38945a58ccd7ef8659e7438eb73
Joel Nider <joel@nider.org>
2018-08-12 01:35:39 +0000

    debugging - Use ' instead of "

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-//echo "<HTML>\n";
+echo "<HTML>\n";
 echo "Actions menu<BR><BR>\n";
-echo "<LINK HREF="community.php?func=add">Add Community</LINK><BR>\n";
-//echo "</HTML>\n";
+echo "<LINK HREF='community.php?func=add'>Add Community</LINK><BR>\n";
+echo "</HTML>\n";
 ?>
