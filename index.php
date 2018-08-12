17aaf28483ce766c6a4d489afc2b98204b15e78d
Joel Nider <joel@nider.org>
2018-08-12 01:54:45 +0000

    Add decorations
    The head tag and dtd to see why the link isn't working right.

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+echo "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
 echo "<HTML>\n";
+echo "<head>\n";
+echo "<title>Actions Menu</title>\n";
+echo "</head>\n";
 echo "<body>\n";
-echo "Actions menu<BR><BR>\n";
 echo "<link href=\"community.php?func=add\">Add Community</link><BR>\n";
 echo "</body>\n";
 echo "</HTML>\n";
