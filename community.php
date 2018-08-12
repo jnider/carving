1a8feb28265088b502ea454551ac19ffda3dd4df
Joel Nider <joel@nider.org>
2018-08-12 01:26:08 +0000

    Add 'community' page
    First draft - use to add a new community entry to the db.
    
    Signed-off-by: Joel Nider <joel@nider.org>

---
+++ community.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<HTML>\n";
+echo "Add community\n";
+echo "<FORM>\n";
+echo "Name:<INPUT type='text' name='name'>\n";
+echo "Alternative Name:<INPUT type='text' name='alt_name'>\n";
+echo "<INPUT type='submit' value='Add'>\n";
+echo "</FORM>\n";
+echo "</HTML>\n";
+?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "Hello World!";
-
+echo "<HTML>\n";
+echo "Actions menu<BR><BR>\n";
+echo "<LINK HREF="community.php?func=add">Add Community</LINK><BR>\n";
+echo "</HTML>\n";
 ?>
