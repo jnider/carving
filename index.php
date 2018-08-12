bce607b4ee1e7880ad1f093a89d05dcf117399c5
Joel Nider <joel@nider.org>
2018-08-12 01:46:30 +0000

    Add body tag
    Page loads, but link doesn't show up. This should fix it.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 echo "<HTML>\n";
+echo "<body>\n";
 echo "Actions menu<BR><BR>\n";
-echo "<LINK HREF='community.php?func=add'>Add Community</LINK><BR>\n";
+echo "<link href=\"community.php?func=add\">Add Community</link><BR>\n";
+echo "</body>\n";
 echo "</HTML>\n";
 ?>
