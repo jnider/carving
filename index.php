5f4b818eed4379af7247f9c84201c5b108c750db
Joel Nider <joel@nider.org>
2018-08-12 01:32:20 +0000

    debugging - put the link back

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 //echo "<HTML>\n";
 echo "Actions menu<BR><BR>\n";
-//echo "<LINK HREF="community.php?func=add">Add Community</LINK><BR>\n";
+echo "<LINK HREF="community.php?func=add">Add Community</LINK><BR>\n";
 //echo "</HTML>\n";
 ?>
