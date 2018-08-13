f08364947ae1bf1d91baf655d7c20bd48e18576a
Joel Nider <joel@nider.org>
2018-08-13 00:37:53 +0000

    debug - removing even more

--- community.php
+++ community.php
@@ -7,22 +7,20 @@
 function show_form_add()
 {
 	//echo "<head><title>Add community</title></head>\n";
-	//echo "<body>\n";
 	echo "<form method=\"post\" action=\"community.php\">\n";
-	echo "<input type=\"hidden\" name=\"action\" value=\"add\" />\n";
+	echo "<input type=\"hidden\" name=\"action\" value=\"add\">\n";
 	echo "Name:<input type=\"text\" name=\"name\">\n";
 	echo "Alternative Name:<input type=\"text\" name=\"alt_name\"><BR>\n";
 	echo "<input type=\"submit\" value=\"Add\">\n";
 	echo "</form>\n";
-	//echo "</body>\n";
 }
 
 function add($name, $alt_name)
 {
 	//echo "<body>\n";
 	//echo "Adding community $name, $alt_name<BR>\n";
-	$query = "insert into community_lu (name) values (\"$1\")";
-	$params = {$name};
+	//$query = "insert into community_lu (name) values (\"$1\")";
+	//$params = {$name};
 	//pg_query_params($db, $query, $params);
 	//echo "</body>\n";
 	return 0;
@@ -53,11 +51,11 @@ case "form_add":
 	break;
 
 case "add":
-	$ret=add($community_name, $alt_name);
-	if ($ret==0)
-		echo "Community added ok\n";
-	else
-		echo "Community not added: $ret\n";
+	//$ret=add($community_name, $alt_name);
+	//if ($ret==0)
+	//	echo "Community added ok\n";
+	//else
+	//	echo "Community not added: $ret\n";
 	break;
 
 default:
