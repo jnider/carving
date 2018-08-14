a71a40c1c648340fdefc1578239b1883292ce07a
Joel Nider <joel@nider.org>
2018-08-14 04:37:04 +0000

    Add login form
    This is now in the index page, but should be moved later once it works.

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 
+function show_login_form()
+{
+echo "<HTML>\n";
+echo "<head>\n";
+echo "<title>Login</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "<form method=\"post\" action=\"index.php\">\n";
+echo "<input type=\"hidden\" name=\"action\" value=\"login\" />\n";
+echo "<input type=\"text\" name=\"name\" />\n";
+echo "<input type=\"password\" name=\"password\" />\n";
+echo "<input type=\"submit\" value=\"Login\" />\n";
+echo "</form>\n";
+echo "</body>\n";
+echo "</HTML>\n";
+}
+
 session_start();
+
+if (!isset($_SESSION['username']))
+{
+	login();
+}
 
 echo "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
 echo "<HTML>\n";
@@ -12,12 +34,6 @@ echo "<form method=\"post\" action=\"community.php\">\n";
 echo "<input type=\"hidden\" name=\"action\" value=\"form_add\" />\n";
 echo "<input type=\"submit\" value=\"Add Community\" />\n";
 echo "</form>\n";
-
-if (!isset($_SESSION['username']))
-{
-	echo "Not logged in\n";
-}
-
 echo "</body>\n";
 echo "</HTML>\n";
 ?>
