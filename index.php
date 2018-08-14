4efaeb9e791152f7631d490798385b42949a3fde
Joel Nider <joel@nider.org>
2018-08-14 04:52:16 +0000

    check login credentials
    Use hardcoded values for now.

--- index.php
+++ index.php
@@ -8,11 +8,30 @@ echo "</head>\n";
 echo "<body>\n";
 echo "<form method=\"post\" action=\"index.php\">\n";
 echo "<input type=\"hidden\" name=\"action\" value=\"login\" />\n";
-echo "<input type=\"text\" name=\"name\" />\n";
+echo "<input type=\"text\" name=\"username\" />\n";
 echo "<input type=\"password\" name=\"password\" />\n";
 echo "<input type=\"submit\" value=\"Login\" />\n";
 echo "</form>\n";
 echo "</body>\n";
+}
+
+function login()
+{
+	if ($user == "joel" && $password == "nider")
+		return 0;
+	return 1;
+}
+
+if (isset($_POST['action']))
+{
+	if ($_POST['action'] == "login")
+	{
+		$ret = login($_POST['username'], $_POST['password']);
+		if ($ret)
+			echo "Error logging in\n";
+		else
+			header('Location: index.php');
+	}
 }
 
 session_start();
