237d9bc60966940e40f17e8e660baf27d8cbe5c5
Joel Nider <joel@nider.org>
2018-08-15 03:36:56 +0000

    Add 'add user' button
    To add a user. Doesn't do anything yet - just the button.

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ function login($username, $password)
 	return 1;
 }
 
+// check to see if a user is logging in
 if (isset($_POST['action']))
 {
 	if ($_POST['action'] == "login")
@@ -54,10 +55,16 @@ else
 	echo "</head>\n";
 	echo "<body>\n";
 
-	// add community button
+	// 'add community' button
 	echo "<form method=\"post\" action=\"community.php\">\n";
 	echo "<input type=\"hidden\" name=\"action\" value=\"form_add\" />\n";
 	echo "<input type=\"submit\" value=\"Add Community\" />\n";
+	echo "</form>\n";
+
+	// 'add user' button
+	echo "<form method=\"post\" action=\"user.php\">\n";
+	echo "<input type=\"hidden\" name=\"action\" value=\"form_add\" />\n";
+	echo "<input type=\"submit\" value=\"Add User\" />\n";
 	echo "</form>\n";
 
 	// 'logout' button
