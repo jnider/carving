486df3b661854025ab134829f4a1777de2bb8e15
Joel Nider <joel@nider.org>
2018-08-16 04:26:49 +0000

    Use slashes facing the right way

--- art.php
+++ art.php
@@ -52,13 +52,13 @@ function show_form_add_art($db)
 function show_art($db)
 {
 	echo "<div class=\"left\">\n";
-	echo "<img src=\"images\\arrow-left.png\">\n";
+	echo "<img src=\"images/arrow-left.png\">\n";
 	echo "</div>\n";
 
 	echo "Some text\n";
 
 	echo "<div class=\"right\">\n";
-	echo "<img src=\"images\\arrow-right.png\">\n";
+	echo "<img src=\"images/arrow-right.png\">\n";
 	echo "</div>\n";
 }
 
