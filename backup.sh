2b28e9ba128661f6ad02990e6ae4261847e8f6d2
Joel Nider <joel@nider.org>
2021-04-25 11:19:11 -0700

    backup: archive backups
    Backups can be initiated from the admin page. Both tar.bz2 and .zip
    files are created for convenience. They are placed in an output
    directory that is not accessible by the web server. Other scripts
    sync this location to other servers periodically for safekeeping.
    
    Signed-off-by: Joel Nider <joel@nider.org>

--- backup.sh
+++ backup.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-filename="carving-db-$(date +%F).sql"
+# We don't want to leave these files in an area accessible from the
+# web - some unauthorized person may get ahold of them
 
-echo $filename
-pg_dump -U postgres carving > ../$filename
+outpath=/mnt/data/public/carving
+
+# make sure the timestamp matches in all files by only generating it once
+timestamp=$(date +%F)
+db_filename="carving-db-$timestamp.sql"
+tar_filename="carving-db-$timestamp.tar.bz2"
+zip_filename="carving-db-$timestamp.zip"
+
+# dump the database structure and data
+pg_dump -U postgres carving > $db_filename
+if [ $? != 0 ]; then
+	echo "Dump failed"
+	exit
+fi
+
+# create archive including the database and associated pictures
+tar cjf $tar_filename $db_filename pictures
+mv $tar_filename $outpath
+
+# create zip for Windows users
+zip $zip_filename -q -r -b . $db_filename pictures
+chmod 644 $zip_filename
+mv $zip_filename $outpath
+
+# remove the database dump once it is archived
+rm $db_filename
+
+echo $outpath
